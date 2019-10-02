0c5da0be71d5a37aca3c3fc9925633b7b5a6e8f2
Stephan Nordnes Eriksen <stephanruler@gmail.com>
2019-10-02 18:24:49 +0200

    feat: test release auto github

--- main.js
+++ main.js
@@ -128,7 +128,7 @@ function createWindow() {
 	win.loadFile('index.html')
 	// win.setMenu(null)
 	// Open the DevTools.
-	win.webContents.openDevTools()
+	// win.webContents.openDevTools()
 
 	// Emitted when the window is closed.
 	win.on('closed', () => {
